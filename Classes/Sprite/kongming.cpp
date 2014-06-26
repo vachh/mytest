bcb23914002d203229d8da21907567d27601efcd
vachh <435829403@qq.com>
2014-06-26 09:57:14 +0800

    ht
    hh

--- Classes/Sprite/kongming.cpp
+++ Classes/Sprite/kongming.cpp
@@ -10,17 +10,14 @@ kongming* kongming::create(CCPoint point)
 }
 bool kongming::init(CCPoint point)
 {	
-	CCLog("%s BEGINS",__FUNCTION__);
-	state=right;
+	_speed=70.0f;
+	state=kongming_dir.right;
 	winSize = CCDirector::sharedDirector()->getWinSize();
 	aniCache=CCAnimationCache::sharedAnimationCache();
 	///////////////////////
 	frameCache=CCSpriteFrameCache::sharedSpriteFrameCache();
 	frameCache->addSpriteFramesWithFile("kongming.plist");
 
-    //spriteSheet=CCSpriteBatchNode::create("kongming.png");
-	//this->addChild(spriteSheet);
-	
 	for (int i=1;i<=4;i++)
 	{	
 		CCArray *walkAnimFrames = CCArray::createWithCapacity(4); 
@@ -28,123 +25,118 @@ bool kongming::init(CCPoint point)
 			for(int j = 1; j <= 4; j++) { 
 				char str[20]={0};
 				sprintf(str,"kongming_0%d.png", j+(i-1)*4);
-				CCLog("%s BEGINS","spriteSheet_add");
+				//CCLog("%s BEGINS","spriteSheet_add");
 				walkAnimFrames->addObject(frameCache->spriteFrameByName(str));
-				CCLog("%s BEGINS","spriteSheet_add_object");
-			} 
-			
+				//CCLog("%s BEGINS","spriteSheet_add_object");
+			} 	
 			CCAnimation *walkAnim = CCAnimation::createWithSpriteFrames(walkAnimFrames, 0.2f); 
 			char str_be[20];
 			sprintf(str_be,"kongming_0%d.png", 1+(i-1)*4);
-			if (i==down)
-			{
-				aniCache->addAnimation(walkAnim,"down");
-				spriteDown=CCSprite::createWithSpriteFrame( frameCache->spriteFrameByName(str_be));
-				spriteDown->setPosition(ccp(0,0));
-				CC_SAFE_RETAIN(spriteDown);
-			}
-			else if (i==left)
-			{
-				aniCache->addAnimation(walkAnim,"left");
-				spriteLeft=CCSprite::createWithSpriteFrame(frameCache->spriteFrameByName(str_be));
-				spriteLeft->setPosition(ccp(0,0));
-				CC_SAFE_RETAIN(spriteLeft);
-			}
-			else if (i==right)
-			{
-				aniCache->addAnimation(walkAnim,"right");
-				spriteRight=CCSprite::createWithSpriteFrame(frameCache->spriteFrameByName(str_be)) ;
-				spriteRight->setPosition(point);
-				CC_SAFE_RETAIN(spriteRight);
-			}
-			else if (i==up)
-			{
-				aniCache->addAnimation(walkAnim,"up");
-				spriteUp=CCSprite::createWithSpriteFrame(frameCache->spriteFrameByName(str_be));
-				spriteUp->setPosition(ccp(0,0));
-				CC_SAFE_RETAIN(spriteUp);
+			switch(i){
+				case kongming_dir.down:
+					aniCache->addAnimation(walkAnim,"down");
+					spriteDown=CCSprite::createWithSpriteFrame( frameCache->spriteFrameByName(str_be));
+					CC_SAFE_RETAIN(spriteDown);
+					break;
+				case kongming_dir.left:
+					aniCache->addAnimation(walkAnim,"left");
+					spriteLeft=CCSprite::createWithSpriteFrame(frameCache->spriteFrameByName(str_be));
+					CC_SAFE_RETAIN(spriteLeft);
+					break;
+				case kongming_dir.right:
+					aniCache->addAnimation(walkAnim,"right");
+					spriteRight=CCSprite::createWithSpriteFrame(frameCache->spriteFrameByName(str_be)) ;
+					CC_SAFE_RETAIN(spriteRight);
+					break;	
+				case kongming_dir.up:	
+					aniCache->addAnimation(walkAnim,"up");
+					spriteUp=CCSprite::createWithSpriteFrame(frameCache->spriteFrameByName(str_be));
+					CC_SAFE_RETAIN(spriteUp);
+					break;
 			}
 			
-	//	walkAnimFrames->removeAllObjects();
-
 	}
 	CCAnimate* animate=CCAnimate::create(aniCache->animationByName("right"));
+	spriteRight->setPosition(point);
+	spriteWidth=spriteRight->getContentSize().width;
+	spriteHeight=spriteRight->getContentSize().height;
 	this->addChild(spriteRight);
 	spriteRight->runAction(CCRepeatForever::create(animate));
-	_speedX=-70.0f;
 	return true;
 }
-void kongming::setPos(CCPoint point){
-	if (state==down)
-	{
-		this->getSpriteDown()->setPosition(point);
+void kongming::_setPos(float s){
+	CCPoint pos=this->getPos();
+	CCPoint nowPos;
+	CCSprite* tmpSprite;
+	switch(state){
+			case kongming_dir.down:
+				nowPos=ccp(pos.x,pos.y-s);
+				tmpSprite=this->getSpriteDown();
+				break;
+			case kongming_dir.left:
+				tmpSprite=this->getSpriteLeft();
+				nowPos=ccp(pos.x-s,pos.y);
+				break;
+			case kongming_dir.right:
+				tmpSprite=this->getSpriteRight();
+				nowPos=ccp(pos.x+s,pos.y);
+				break;
+			case kongming_dir.up:
+				tmpSprite=this->getSpriteUp();
+				nowPos=ccp(pos.x,pos.y+s);
+				break;
 	}
-	else	if (state==left)
-	{
-		this->getSpriteLeft()->setPosition(point);
-	}
-	else if (state==right)
-	{
-		this->getSpriteRight()->setPosition(point);
-	}
-	else if (state==up)
-	{
-		this->getSpriteUp()->setPosition(point);
-	}
+	if (nowPos.x<=this->spriteWidth/2||nowPos.x>=winSize.width-spriteWidth/2||
+		nowPos.y<=spriteHeight/2||nowPos.y>=winSize.height-spriteHeight/2)
+		return;
+		tmpSprite->setPosition(nowPos);
 }
 CCPoint kongming::getPos(){
-	if (state==down)
-	{
+	switch(state){
+	case kongming_dir.down:
 		return this->getSpriteDown()->getPosition();
-	}
-	else	if (state==left)
-	{
+	case kongming_dir.left:
 		return this->getSpriteLeft()->getPosition();
-	}
-	else if (state==right)
-	{
+	case kongming_dir.right:
 		return this->getSpriteRight()->getPosition();
-	}
-	else if (state==up)
-	{
+	case kongming_dir.up:
 		return this->getSpriteUp()->getPosition();
 	}
 }
-void kongming::ToDown(){
-	CCPoint point=this->getPos();
-	state=down;
+void kongming::Turn(int dir){
+	CCPoint pos=this->getPos();
 	this->removeAllChildren();
-	CCAnimate* animate=CCAnimate::create(aniCache->animationByName("down"));
-	spriteDown->setPosition(point);
-	this->addChild(spriteDown);
-	spriteDown->runAction(CCRepeatForever::create(animate));
+	CCAnimate* animate;
+	CCSprite* tmpSprite;
+	switch (dir)
+	{
+	case kongming_dir.down:
+		state=kongming_dir.down;
+		animate=CCAnimate::create(aniCache->animationByName("down"));
+		tmpSprite=this->getSpriteDown();
+		break;
+	case kongming_dir.left:
+		state=kongming_dir.left;
+		animate=CCAnimate::create(aniCache->animationByName("left"));
+		tmpSprite=this->getSpriteLeft();
+		break;
+	case kongming_dir.right:
+		state=kongming_dir.right;
+		animate=CCAnimate::create(aniCache->animationByName("right"));
+		tmpSprite=this->getSpriteRight();
+		break;
+	case kongming_dir.up:
+		state=kongming_dir.up;
+		animate=CCAnimate::create(aniCache->animationByName("up"));
+		tmpSprite=this->getSpriteUp();
+		break;
+	}
+	tmpSprite->setPosition(pos);
+	this->addChild(tmpSprite);
+	tmpSprite->runAction(CCRepeatForever::create(animate));
 }
-void kongming::ToLeft(){
-	CCPoint point=this->getPos();
-	state=left;
-	this->removeAllChildren();
-	CCAnimate* animate=CCAnimate::create(aniCache->animationByName("left"));
-	spriteLeft->setPosition(point);
-	this->addChild(spriteLeft);
-	spriteLeft->runAction(CCRepeatForever::create(animate));
-}
-void kongming::ToRight(){
-	CCPoint point=this->getPos();
-	this->removeAllChildrenWithCleanup(false);
-	state=right;
-	CCAnimate* animate=CCAnimate::create(aniCache->animationByName("right"));
-	spriteRight->setPosition(point);
-	this->addChild(spriteRight);
-	spriteRight->runAction(CCRepeatForever::create(animate));
-}
-void kongming::ToUp(){
-	cocos2d::CCPoint point=this->getPos();
-	this->removeAllChildrenWithCleanup(false);
-	state=up;
-	CCAnimate* animate=CCAnimate::create(aniCache->animationByName("up"));
-	spriteUp->setPosition(point);
-	this->addChild(spriteUp);
-	spriteUp->runAction(CCRepeatForever::create(animate));
+void kongming::update(float s){
+	this->_setPos(s);
 }
 kongming::~kongming(){
 	CCAnimationCache::purgeSharedAnimationCache();
